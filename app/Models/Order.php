8d56a62c3b8436c841dff0e5a25818fbbd83ce4d
ramdant29 <ramdanthalib3@gmail.com>
2026-06-10 14:27:51 +0700

    Mengubah Cart dan Order Model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,10 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    protected $fillable = ['customer_name', 'customer_email', 'product_id', 'quantity', 'total_price', 'status'];
+    protected $fillable = ['customer_name', 'customer_email', 'product_id', 'quantity', 'total_price', 'voucher_code', 'discount_amount', 'status'];
 
     public function product()
     {
         return $this->belongsTo(Product::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
     }
 }
